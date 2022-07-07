4a95916e688706103ad3a88c3535fcb4339017fa
Roman Shevchenko <rshevchenko@me.com>
2022-07-07 12:22:42 +0300

    Initial commit

---
+++ src/main/java/com/github/romashe/sntmgmt/SntmgmtApplication.java
@@ -0,0 +1,13 @@
+package com.github.romashe.sntmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SntmgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SntmgmtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/romashe/sntmgmt/SntmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.romashe.sntmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SntmgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
