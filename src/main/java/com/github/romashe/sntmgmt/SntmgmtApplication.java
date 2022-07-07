f841253fe10fcb0bc37b98381b59bfe3fd4c0a78
Roman Shevchenko <rshevchenko@me.com>
2022-07-07 14:09:41 +0300

    Added HelloWorld and Dockerfile

--- src/main/java/com/github/romashe/sntmgmt/SntmgmtApplication.java
+++ src/main/java/com/github/romashe/sntmgmt/SntmgmtApplication.java
@@ -2,9 +2,17 @@ package com.github.romashe.sntmgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SntmgmtApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SntmgmtApplication.class, args);
